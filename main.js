a7b0c8417ccc79a0afa63b67ac39887a4341fedf
Aissa Feldmann <aissaf@umich.edu>
2020-08-20 16:19:30 -0400

    "basic posting functionality"

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// your code here
+let button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click", function () {
+  let userName = document.getElementById("name").value;
+  let userPost = document.getElementById("message").value;
+
+  let userNameAndPost = userName.concat(": " + userPost);
+
+  let userPostElement = document.createTextNode(userNameAndPost);
+  let li = document.createElement("li");
+  li.appendChild(userPostElement);
+
+  document.getElementById("posts").append(li);
+});
